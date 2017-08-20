af2396f5291d5772a990603260dea2a197933f4e
yqsy <yqsy021@126.com>
2017-08-20 16:54:18 +0800

    use yaml

--- qiniu_tools.py
+++ qiniu_tools.py
@@ -6,8 +6,7 @@ import qiniu.config
 import argparse
 import sys
 import os
-
-
+import yaml
 
 access_key = 'access_key'
 secret_key = 'secret_key'
@@ -22,8 +21,11 @@ def parseopts():
 
     upload_parser.add_argument('file', nargs='+', help='files you want to upload')
 
-    upload_parser.add_argument('-b', '--bucket', action='store', required=True, type=str,
+    upload_parser.add_argument('-b', '--bucket', action='store', required=True,
                                metavar='<bucket>')
+
+    upload_parser.add_argument('-k', '--keyfile', action='store', required=True,
+                               metavar='<.yaml>')
 
     options = arguementparser.parse_args()
 
@@ -31,7 +33,16 @@ def parseopts():
 
 
 def upload(options):
-    q = Auth(access_key, secret_key)
+    if not os.path.isfile(options.keyfile):
+        sys.exit('{} is not a file'.format(options.keyfile))
+
+    key_map = []
+    with open(options.keyfile) as f:
+        key_map = yaml.safe_load(f)
+
+    print(key_map)
+
+    q = Auth(key_map['AccessKey'], key_map['SecretKey'])
 
     for file in options.file:
         print('dealing with {}'.format(file))
